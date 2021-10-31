05c3d37924479083e144a086efb48c403546e06c
antonKuk <akuk211@mywhitecliffe.com>
2021-11-01 12:24:14 +1300

    finished

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace ArrayTasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] lettersArr = {'A','B','D','F'};
+            foreach (char letter in lettersArr)
+            {
+                Console.WriteLine($"{letter}");
+            }
+            char[] newLetters = new char[5];
+            Array.Copy(lettersArr, newLetters, 2);
+            newLetters[2] = 'C';
+            Array.Copy(lettersArr,2, newLetters,3, 2);
+            foreach (char letter in newLetters)
+            {
+                Console.Write($"{letter}");
+            }
         }
     }
 }
